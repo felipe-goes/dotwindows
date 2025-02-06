7dde8de741d964afc44b49c9612e797d8785ee00
Felipe Goes <ogoesfelipe@gmail.com>
2025-02-06 18:18:08 -0300

    fix: fix bugs

--- Documents/PowerShell/scripts/config.ps1
+++ Documents/PowerShell/scripts/config.ps1
@@ -1,40 +1,13 @@
 function config{
   param(
-    [Parameter(
-      Mandatory=$True,
-      Position=0
-    )]
-    [String]
-    $firstCommand,
-
-    [Parameter(
-      Mandatory=$False,
-      Position=1
-    )]
-    [String[]]
-    $listArgs
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$commands
   )
 
   $gitDir = "$home\.dotwindows"
   $workTree = $home
 
-  if ($listArgs.Length -eq 0){
-    git --git-dir=$gitDir --work-tree=$workTree $firstCommand
-  } else {
-
-    $argsString = ""
-    $count = 0
-    foreach($listArg in $listArgs){
-      if ($count -gt 0) {
-        $argsString = $argsString + " " + $listArg
-      } else {
-        $argsString = $listArg
-        $count++
-      }
-    }
-
-    git --git-dir=$gitDir --work-tree=$workTree $firstCommand $argsString
-  }
+  git --git-dir=$gitDir --work-tree=$workTree $commands
 
 }
 
